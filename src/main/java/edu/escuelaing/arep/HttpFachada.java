f58347e871fcbcfae50a11d4a2b4e0d5ec20bdc8
LaaSofiaa <laura.gil-c@mail.escuelaing.edu.co>
2025-02-21 10:49:21 -0500

    mitad de httpfachada

--- src/main/java/edu/escuelaing/arep/HttpFachada.java
+++ src/main/java/edu/escuelaing/arep/HttpFachada.java
@@ -1,4 +1,88 @@
 package edu.escuelaing.arep;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class HttpFachada {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(35000);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 35000.");
+            System.exit(1);
+        }
+
+        boolean running= true;
+        while (running) {
+            Socket clientSocket = null;
+            try {
+                System.out.println("Listo para recibir ...");
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.err.println("Accept failed.");
+                System.exit(1);
+            }
+            PrintWriter out = new PrintWriter(
+                    clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+            String inputLine, outputLine;
+
+            boolean isFirstLine = true;
+            String request = "";
+            while ((inputLine = in.readLine()) != null) {
+                //System.out.println("Recibí: " + inputLine);
+                if (isFirstLine) {
+                    request = inputLine;
+                    isFirstLine = false;
+                }
+                if (!in.ready()) {
+                    break;
+                }
+            }
+
+            String path = request.split(" ")[1];
+            System.out.println(path);
+            System.out.println(request);
+
+
+            if(path.startsWith("/cliente")) {
+                outputLine= getStaticFile();
+            }else if(path.startsWith("/consulta")){
+                outputLine= connectHttpServer(path);
+            }else{
+                outputLine ="HTTP/1.1 200 OK\r\n"
+                        + "Content-Type: text/html\r\n"
+                        + "\r\n"
+                        + "<!DOCTYPE html>" +
+                        "<html>" +
+                        "<head>" +
+                        "<meta charset=\"UTF-8\">" +
+                        "<title>HttpServer</title>\n" +
+                        "</head>" +
+                        "<body>" +
+                        "<h1>Funciona Else HttpServer</h1>" +
+                        "</body>" +
+                        "</html>";
+            }
+            out.println(outputLine);
+            out.close();
+            in.close();
+            clientSocket.close();
+        }
+        serverSocket.close();
+    }
+
+    private static String getStaticFile() {
+        return "";
+    }
+
+    private static String connectHttpServer(String path) {
+        return "";
+    }
 }
